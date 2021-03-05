c0d0eb3a296a78c7b687ffcf1babfd88040c1646
kazupin0930 <jurkt.kantaros@icloud.com>
2021-03-05 13:43:08 +0900

    修正

--- app/models/chat.rb
+++
@@ -1,5 +0,0 @@
-class Chat < ApplicationRecord
-
-  belongs_to :pet
-  has_many   :users
-end
--- test/controllers/chats_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ChatsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get chats_new_url
-    assert_response :success
-  end
-
-end
