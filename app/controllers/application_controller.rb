58b3d71241a6fb3650d9ebdfe7a4ddf38ae6a424
kazupin0930 <jurkt.kantaros@icloud.com>
2021-03-06 18:35:17 +0900

    時刻の日本語表記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :keep_pet, :number_of_pets])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'minato' && password == '0604'
+    end
   end
 end
