67c9a5aa2da1d687fe8dc55e82930f95f7faf7bc
kazupin0930 <jurkt.kantaros@icloud.com>
2021-03-10 00:02:36 +0900

    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'pet_with'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, 'git@egithub.com:kazupin0930/pet_with.git'
+set :repo_url, 'git@github.com:kazupin0930/pet_with.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
