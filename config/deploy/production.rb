321a7bd7f5f19e690814aaa522dbfeaeb37cd4e1
kazupin0930 <jurkt.kantaros@icloud.com>
2021-03-12 01:02:45 +0900

    変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '35.73.72.206', user: 'ec2-user', roles: %w{app db web}
+# server '35.73.72.206', user: 'ec2-user', roles: %w{app db web}
